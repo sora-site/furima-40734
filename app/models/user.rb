18b6028c1905b75dc07d14e6af7bd493d34b875e
sora-site <soshi.1999.13@gmail.com>
2024-06-09 15:43:51 +0900

    正規表現を用いたvalidationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,16 @@ class User < ApplicationRecord
   # has_many :items
   # has_many :purchases
   # has_many :comments
-  validates :encrypted_password, length: { minimum: 6 }
+
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-z]\d)[a-z\d]{6,128}\z/ # 文字数はデフォルト
+  VALID_KANJI_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  VALID_KANA_NAME_REGEX = /\A[ァ-ヶ一]+\z/
+
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
-  validates :kanji_name_sei, presence: true
-  validates :kanji_name_mei, presence: true
-  validates :kana_name_sei, presence: true
-  validates :kana_name_mei, presence: true
+  validates :kanji_name_sei, presence: true, format: { with: VALID_KANJI_NAME_REGEX }
+  validates :kanji_name_mei, presence: true, format: { with: VALID_KANJI_NAME_REGEX }
+  validates :kana_name_sei, presence: true, format: { with: VALID_KANA_NAME_REGEX }
+  validates :kana_name_mei, presence: true, format: { with: VALID_KANA_NAME_REGEX }
   validates :birthday, presence: true
 end
