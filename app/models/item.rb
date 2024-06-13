ebed5c58c895fe4d640580a2db7029c56c92cc01
sora-site <soshi.1999.13@gmail.com>
2024-06-14 00:49:17 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,18 +11,18 @@ class Item < ApplicationRecord
   validates :item_description, presence: true
   validates :category_id, presence: true
   validates :price, presence: true, format: { with: VALID_PRICE_REGEX },
-                    numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :condition_id, presence: true
   validates :shipping_cost_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipping_date_id, presence: true
   validates :image, presence: true
-  # ジャンルの選択が「---」の時は保存できないようにする（activehash）
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_cost_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :shipping_date_id, numericality: { other_than: 1 }
+  # ジャンルの選択が「---」の時は保存できないようにする／設定したIDより大きい時は保存できないようにする。（activehash）
+  validates :category_id, numericality: { other_than: 1, less_than: 12 }
+  validates :condition_id, numericality: { other_than: 1, less_than: 8 }
+  validates :shipping_cost_id, numericality: { other_than: 1, less_than: 4 }
+  validates :prefecture_id, numericality: { other_than: 1, less_than: 49 }
+  validates :shipping_date_id, numericality: { other_than: 1, less_than: 5 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
