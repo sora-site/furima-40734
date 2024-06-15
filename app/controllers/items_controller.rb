2980d437b3c87d7c8147ce9f80d16be837550ac5
sora-site <soshi.1999.13@gmail.com>
2024-06-15 16:27:28 +0900

    rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,9 +25,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    if @item.user_id != current_user.id
-      redirect_to root_path
-    end
+    return unless @item.user_id != current_user.id
+
+    redirect_to root_path
   end
 
   def update
