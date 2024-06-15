8234873c3524c341f8202b73b3d3117496e79b35
sora-site <soshi.1999.13@gmail.com>
2024-06-15 16:59:29 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, only: [:update, :edit]
+  before_action :move_to_index, only: [:update, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
